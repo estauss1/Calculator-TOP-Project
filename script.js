563a40a6419e146a4b07cb26ad93282484c2a527
estauss1 <estauss23@gmail.com>
2022-07-27 14:27:07 -0400

    Break large addtoscreen func into smaller funcs and add extra functionality
    Instead of one function being called for all buttons, each category
    of button triggers a seperate smaller function. Add exception handling
    and make it so you can use the answer from prev equation as first number

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const clear = document.querySelector("#clear");
 const equal = document.querySelector("#equal"); 
 const screen  = document.querySelector("#screen");
 
-numbers.forEach((number) => number.addEventListener("click", addToScreen));
+numbers.forEach((number) => number.addEventListener("click", numberPushed));
+operators.forEach((operator) => operator.addEventListener("click", operatorPushed));
+equal.addEventListener("click", equate)
+clear.addEventListener("click", clearAll);
 
 
 let equation = [];
@@ -26,7 +29,9 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function operate(operator, num1, num2){
+function operate(stringNum1, operator, stringNum2){
+    let num1 = Number(stringNum1);
+    let num2 = Number(stringNum2);
     switch(operator){
         case "+":
             return add(num1, num2);
@@ -39,18 +44,60 @@ function operate(operator, num1, num2){
     }
 }
 
-function addToScreen(event){
-    const char = event.target.textContent;
-    screen.textContent += char;
-    let converted = parseInt(char);
-    if(typeof converted == "number"){
-        digitArray.push(converted);
-    } else{   // is an operator
-        let wholeNumber = digitArray.join("");
-        equation.push(wholeNumber);
-        clearArray(digitArray);
-        equation.push(char);
+function numberPushed(event){
+    const digit = event.target.textContent;
+    if(equation.length == 1){       // last answer in equation[] or clear
+        clearAll();
+        screen.textContent = digit;
+        digitArray.push(digit);
+    } else if(equation.length == 2 || equation.length == 0){
+        screen.textContent += digit;
+        digitArray.push(digit);
     }
+
+}
+
+function operatorPushed(event){
+    const operator = event.target.textContent;
+    condenseAndAdd();
+    if(equation.length == 3){
+        let expAns = operate(equation[0], equation[1], equation[2]);
+        screen.textContent = expAns + operator;
+        clearArray(equation);
+        equation.push(expAns);
+        equation.push(operator);
+    } else if(equation.length == 1){
+        screen.textContent += operator;
+        condenseAndAdd();
+        equation.push(operator);
+    }
+}
+
+function equate(event){
+    condenseAndAdd();
+    if(equation.length == 3){
+        const answer = operate(equation[0], equation[1], equation[2]);
+        screen.textContent = answer;
+        clearArray(equation);
+        equation.push(answer);
+    } else {
+        clearAll();
+    }
+
+}
+
+function condenseAndAdd(){
+    if(digitArray.length != 0){
+        const numToAdd = digitArray.join("");
+        clearArray(digitArray);
+        equation.push(numToAdd);
+    }
+}
+
+function clearAll(){
+    screen.textContent = "";
+    clearArray(equation);
+    clearArray(digitArray);
 }
 
 function clearArray(array){
